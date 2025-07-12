e8475ed02ba602be99dee32d19fbf7169ecbe43e
Konstantinos Psyridis <psiridis.konstantinos@gmail.com>
2025-07-13 01:56:27 +0300

    Include a CMakeLists.txt and a dummy header
    In this first commit a CMakeLists.txt, a dummy
    header file with the declaration and the
    corresponding source file of a function
    (which prints a message!) has implemented.
    Also a .gitignore file is included.
    
    Information:
    root dir:
    cmake -S . -B build/ -G Ninja
    cmake --build build/
    cmake --install build/ --prefix "some_location"
    
    The last command will perform the installation
    step for the CMake project.
    The argument --prefix is to change the default
    path and specify our own.
    
    If we know the exact path then the user, that
    actually uses the SiML library can use the
    following command:
    cmake -DCMAKE_PREFIX_PATH="list_of_paths" .......
    -DCMAKE_PREFIX_PATH will look the list of paths
    for dependancies for packages via find_package()

---
+++ include/printing.hpp
@@ -0,0 +1,9 @@
+#ifndef PRINTING_HPP
+#define PRINTING_HPP
+
+namespace SiML
+{
+    void printing();
+}
+
+#endif
---
+++ src/printing.cpp
@@ -0,0 +1,10 @@
+#include "printing.hpp"
+#include <iostream>
+
+namespace SiML
+{
+   void printing() 
+   {
+     std::cout << "This is a test function to check the building process!!!" << std::endl;
+   }
+} // namespace SiML
